b022dc5132176a95963894f5111290c1fbf9c72d
ROBIÑHO <asphalt9fr@gmail.com>
2023-10-29 14:27:19 +0100

    whisperx is full op

--- test.py
+++ test.py
@@ -25,8 +25,6 @@ print(result["segments"]) # after alignment
 # delete model if low on GPU resources
 # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
-
-
 # 3. Assign speaker labels
 #diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device)
 
@@ -38,7 +36,16 @@ print(result["segments"]) # after alignment
 #print(diarize_segments)
 #print(result["segments"]) # segments are now assigned speaker IDs
 
-def write_ass(transcript: Iterator[dict], file: TextIO):
+styles = ["Default", "Tilte-left", "Tilte-right"]
+
+import random
+import os
+from typing import Iterator, TextIO
+
+def filename(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+def write_ass(file: TextIO):
     file.write("[Script Info]\n")
     file.write("ScriptType: v4.00\n")
     file.write("Collisions: Normal\n")
@@ -52,33 +59,26 @@ def write_ass(transcript: Iterator[dict], file: TextIO):
     file.write("\n")
     file.write("[Events]\n")
     file.write("Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text\n")
-
-    for i, segment in enumerate(transcript, start=1):
-        counter = 0
-        start_time = format_timestamp(segment['start'], always_include_hours=True)
-        end_time = format_timestamp(segment['end'], always_include_hours=True)
-        #start_time = formatloc(format_timestamp(segment['start'], always_include_hours=True))
-        #end_time = formatloc(format_timestamp(segment['end'], always_include_hours=True))
-        text = segment['text'].strip().replace('\\', '\\\\').replace('{', '\\{').replace('}', '\\}').replace('-->', '->')
-        delta = abs(convert_ms(start_time) - convert_ms(end_time))
-        if text.count(" ") == 0:
-            delta_word = delta
-        else:
-            delta_word = delta/text.count(" ")//11
-        # 100 u = 1000 ms, we know delta_word ms
-        delay = delta_word
-        boiler = "{\q1\\be1\\b700\shad10\\a11\k"+str(int(delay))+"}"
-        emoji = r" \{\frz345}\u1F468 "
-        text =boiler+text.upper().replace(" "," "+boiler)
-        text = text.split(" ")
-        length = len(text)
-        while counter < length:
+    
+    for s in result["segments"]:
+        sentence_start = s['start']
+        sentence_end = s['end']
+        sentence_text = s['text']
+        for segment in s['words']:
+            word = segment['word']
+            if len(segment)==1:
+                break
+            start = segment['start']
+            end = segment['end']
+            delta = (end - start) * 1000
+            boiler = "{\q1\\be1\\b700\shad10\\a11\k"+str(int(delta))+"}"
+            emoji = r" \{\frz345}\u1F468 "
+            text =boiler+word.upper().replace(" "," "+boiler)
             style = styles[random.randint(0,2)]
-            localcount = random.randint(1, 3)
-            if counter + localcount > length:
-                localcount = length - counter
-            localstart = formatloc(add_n_ms(start_time,counter*delta_word*10))
-            localend = formatloc(add_n_ms(localstart,localcount*delta_word*10))
-            localtext = " ".join(text[counter:counter+localcount])
-            file.write(f"""Dialogue: 0,{localstart},{localend},{style},,50,50,20,,{localtext}"""+  "\n")
-            counter += localcount      
+            file.write(f"""Dialogue: 0,{start},{end},{style},,50,50,20,,{text}"""+  "\n")
+            
+ass_path = "temp/"
+path = "test"
+ass_path = os.path.join(ass_path, f"{filename(path)}.ass")
+with open(ass_path,"w", encoding="utf-8") as ass:
+            write_ass(file=ass)
---
+++ test4test.py
@@ -0,0 +1,80 @@
+words = [{'start': 0.229, 'end': 6.333, 'text': " A chaque fois que tu fais un truc, peut-être que c'est caricatural, mais tu vas toujours un peu penser à... Si ma mère regarde... Je te jure que c'est vrai.", 'words': [{'word': 'A', 'start': 0.229, 'end': 0.269, 'score': 0.253}, {'word': 'chaque', 'start': 0.309, 'end': 0.429, 'score': 0.993}, {'word': 'fois', 'start': 0.449, 'end': 0.549, 'score': 0.867}, {'word': 'que', 'start': 0.569, 'end': 0.649, 'score': 0.539}, {'word': 'tu', 'start': 0.689, 'end': 0.77, 'score': 0.53}, {'word': 'fais', 'start': 0.79, 'end': 0.87, 'score': 0.514}, {'word': 'un', 'start': 0.89, 'end': 0.93, 'score': 0.02}, {'word': 'truc,', 'start': 0.95, 'end': 1.45, 'score': 0.871}, {'word': 'peut-être', 'start': 1.47, 'end': 1.85, 'score': 0.456}, {'word': 'que', 'start': 1.87, 'end': 1.97, 'score': 0.715}, {'word': "c'est", 'start': 1.99, 'end': 2.471, 'score': 0.859}, {'word': 'caricatural,', 'start': 2.511, 'end': 2.971, 'score': 0.804}, {'word': 'mais', 'start': 2.991, 'end': 3.071, 'score': 0.924}, {'word': 'tu', 'start': 3.091, 'end': 3.191, 'score': 0.281}, {'word': 'vas', 'start': 3.211, 'end': 3.291, 'score': 0.639}, {'word': 'toujours', 'start': 3.311, 'end': 3.792, 'score': 0.885}, {'word': 'un', 'start': 3.852, 'end': 3.892, 'score': 0.96}, {'word': 'peu', 'start': 3.912, 'end': 4.032, 'score': 0.829}, {'word': 'penser', 'start': 4.072, 'end': 4.292, 'score': 0.914}, {'word': 'à...', 'start': 4.332, 'end': 4.612, 'score': 0.972}, {'word': 'Si', 'start': 4.692, 'end': 4.792, 'score': 0.631}, {'word': 'ma', 'start': 4.812, 'end': 4.872, 'score': 0.954}, {'word': 'mère', 'start': 4.892, 'end': 5.012, 'score': 0.296}, {'word': 'regarde...', 'start': 5.052, 'end': 5.553, 'score': 0.776}, {'word': 'Je', 'start': 5.593, 'end': 5.653, 'score': 0.845}, {'word': 'te', 'start': 5.673, 'end': 5.833, 'score': 0.555}, {'word': 'jure', 'start': 5.853, 'end': 5.953, 'score': 0.635}, {'word': 'que', 'start': 5.973, 'end': 6.033, 'score': 0.544}, {'word': "c'est", 'start': 6.053, 'end': 6.133, 'score': 0.79}, {'word': 'vrai.', 'start': 6.153, 'end': 6.293, 'score': 0.704}]}, {'start': 6.333, 'end': 13.858, 'text': "Par exemple, il y a des films que je refuse de faire, ou des scènes que je refuse de faire dans certains films, parce que je sais que ma mère... Ah, du coup, tu t'interdis des fois des blagues et tout ?", 'words': [{'word': 'Par', 'start': 6.333, 'end': 6.393, 'score': 0.784}, {'word': 'exemple,', 'start': 6.433, 'end': 6.613, 'score': 0.484}, {'word': 'il', 'start': 6.653, 'end': 6.693, 'score': 0.842}, {'word': 'y', 'start': 6.713, 'end': 6.733, 'score': 0.416}, {'word': 'a', 'start': 6.774, 'end': 6.814, 'score': 0.524}, {'word': 'des', 'start': 6.834, 'end': 6.914, 'score': 0.776}, {'word': 'films', 'start': 6.934, 'end': 7.114, 'score': 0.469}, {'word': 'que', 'start': 7.134, 'end': 7.214, 'score': 0.811}, {'word': 'je', 'start': 7.254, 'end': 7.334, 'score': 0.686}, {'word': 'refuse', 'start': 7.374, 'end': 7.654, 'score': 0.906}, {'word': 'de', 'start': 7.674, 'end': 7.734, 'score': 0.919}, {'word': 'faire,', 'start': 7.774, 'end': 8.555, 'score': 0.922}, {'word': 'ou', 'start': 8.595, 'end': 8.735, 'score': 0.604}, {'word': 'des', 'start': 8.755, 'end': 8.835, 'score': 0.788}, {'word': 'scènes', 'start': 8.855, 'end': 9.075, 'score': 0.557}, {'word': 'que', 'start': 9.095, 'end': 9.175, 'score': 0.827}, {'word': 'je', 'start': 9.215, 'end': 9.275, 'score': 0.822}, {'word': 'refuse', 'start': 9.315, 'end': 9.535, 'score': 0.854}, {'word': 'de', 'start': 9.575, 'end': 9.615, 'score': 0.956}, {'word': 'faire', 'start': 9.635, 'end': 9.755, 'score': 0.873}, {'word': 'dans', 'start': 9.776, 'end': 9.896, 'score': 0.825}, {'word': 'certains', 'start': 9.936, 'end': 10.136, 'score': 0.922}, {'word': 'films,', 'start': 10.156, 'end': 10.276, 'score': 0.167}, {'word': 'parce', 'start': 10.296, 'end': 10.416, 'score': 0.777}, {'word': 'que', 'start': 10.436, 'end': 10.516, 'score': 0.848}, {'word': 'je', 'start': 10.556, 'end': 10.596, 'score': 0.919}, {'word': 'sais', 'start': 10.616, 'end': 10.696, 'score': 0.968}, {'word': 'que', 'start': 10.736, 'end': 10.836, 'score': 0.843}, {'word': 'ma', 'start': 10.856, 'end': 10.976, 'score': 0.897}, {'word': 'mère...', 'start': 11.016, 'end': 12.077, 'score': 0.837}, {'word': 'Ah,', 'start': 12.117, 'end': 12.177, 'score': 0.324}, {'word': 'du', 'start': 12.197, 'end': 12.237, 'score': 0.802}, {'word': 'coup,', 'start': 12.257, 'end': 12.377, 'score': 0.528}, {'word': 'tu', 'start': 12.397, 'end': 12.497, 'score': 0.374}, {'word': "t'interdis", 'start': 12.517, 'end': 12.938, 'score': 0.829}, {'word': 'des', 'start': 12.958, 'end': 13.018, 'score': 0.918}, {'word': 'fois', 'start': 13.038, 'end': 13.158, 'score': 0.7}, {'word': 'des', 'start': 13.178, 'end': 13.258, 'score': 0.759}, {'word': 'blagues', 'start': 13.278, 'end': 13.438, 'score': 0.521}, {'word': 'et', 'start': 13.458, 'end': 13.498, 'score': 0.512}, {'word': 'tout', 'start': 13.518, 'end': 13.738, 'score': 0.28}, {'word': '?'}]}, {'start': 13.858, 'end': 15.159, 'text': 'Ah ouais, complètement.', 'words': [{'word': 'Ah', 'start': 13.858, 'end': 14.178, 'score': 0.412}, {'word': 'ouais,', 'start': 14.218, 'end': 14.579, 'score': 0.292}, {'word': 'complètement.', 'start': 14.599, 'end': 15.039, 'score': 0.446}]}, {'start': 15.159, 'end': 17.08, 'text': "Après, j'ai pas tout bien fait dans ma vie.", 'words': [{'word': 'Après,', 'start': 15.159, 'end': 15.619, 'score': 0.355}, {'word': "j'ai", 'start': 15.639, 'end': 15.719, 'score': 0.183}, {'word': 'pas', 'start': 15.739, 'end': 15.96, 'score': 0.284}, {'word': 'tout', 'start': 16.0, 'end': 16.1, 'score': 0.118}, {'word': 'bien', 'start': 16.14, 'end': 16.32, 'score': 0.539}, {'word': 'fait', 'start': 16.34, 'end': 16.46, 'score': 0.769}, {'word': 'dans', 'start': 16.48, 'end': 16.58, 'score': 0.851}, {'word': 'ma', 'start': 16.6, 'end': 16.68, 'score': 0.731}, {'word': 'vie.', 'start': 16.7, 'end': 17.06, 'score': 0.694}]}, {'start': 17.08, 'end': 17.901, 'text': "J'ai pas tout bien fait dans ma vie.", 'words': [{'word': "J'ai", 'start': 17.08, 'end': 17.16, 'score': 0.516}, {'word': 'pas', 'start': 17.18, 'end': 17.24, 'score': 0.985}, {'word': 'tout', 'start': 17.281, 'end': 17.381, 'score': 0.575}, {'word': 'bien', 'start': 17.401, 'end': 17.521, 'score': 0.706}, {'word': 'fait', 'start': 17.561, 'end': 17.641, 'score': 0.473}, {'word': 'dans', 'start': 17.661, 'end': 17.741, 'score': 0.117}, {'word': 'ma', 'start': 17.761, 'end': 17.801, 'score': 0.002}, {'word': 'vie.', 'start': 17.821, 'end': 17.881, 'score': 0.01}]}, {'start': 17.901, 'end': 24.525, 'text': "Il y a des sketchs que... C'est non pas que des sketchs que je regrette, mais en tout cas que j'aurais pas fait comme ça aujourd'hui.", 'words': [{'word': 'Il', 'start': 17.901, 'end': 17.941, 'score': 0.473}, {'word': 'y', 'start': 17.961, 'end': 17.981, 'score': 0.499}, {'word': 'a', 'start': 18.001, 'end': 18.061, 'score': 0.446}, {'word': 'des', 'start': 18.081, 'end': 18.141, 'score': 0.743}, {'word': 'sketchs', 'start': 18.161, 'end': 18.461, 'score': 0.479}, {'word': 'que...', 'start': 18.481, 'end': 19.922, 'score': 0.705}, {'word': "C'est", 'start': 19.942, 'end': 20.042, 'score': 0.436}, {'word': 'non', 'start': 20.643, 'end': 20.743, 'score': 0.925}, {'word': 'pas', 'start': 20.763, 'end': 20.843, 'score': 0.834}, {'word': 'que', 'start': 20.863, 'end': 20.923, 'score': 0.84}, {'word': 'des', 'start': 20.943, 'end': 21.003, 'score': 0.666}, {'word': 'sketchs', 'start': 21.023, 'end': 21.283, 'score': 0.391}, {'word': 'que', 'start': 21.303, 'end': 21.403, 'score': 0.812}, {'word': 'je', 'start': 21.443, 'end': 21.523, 'score': 0.69}, {'word': 'regrette,', 'start': 21.563, 'end': 21.824, 'score': 0.802}, {'word': 'mais', 'start': 21.864, 'end': 21.944, 'score': 0.33}, {'word': 'en', 'start': 21.984, 'end': 22.024, 'score': 0.038}, {'word': 'tout', 'start': 22.044, 'end': 22.144, 'score': 0.274}, {'word': 'cas', 'start': 22.164, 'end': 22.224, 'score': 0.0}, {'word': 'que', 'start': 22.244, 'end': 22.304, 'score': 0.299}, {'word': "j'aurais", 'start': 22.324, 'end': 22.524, 'score': 0.631}, {'word': 'pas', 'start': 22.544, 'end': 22.624, 'score': 0.837}, {'word': 'fait', 'start': 22.644, 'end': 22.764, 'score': 0.783}, {'word': 'comme', 'start': 22.784, 'end': 22.924, 'score': 0.858}, {'word': 'ça', 'start': 22.944, 'end': 23.465, 'score': 0.684}, {'word': "aujourd'hui.", 'start': 23.505, 'end': 23.845, 'score': 0.866}]}, {'start': 24.525, 'end': 25.806, 'text': "Il y a des sketchs que j'ai faits, ouais, pas ouf.", 'words': [{'word': 'Il', 'start': 24.525, 'end': 24.585, 'score': 0.408}, {'word': 'y', 'start': 24.605, 'end': 24.625, 'score': 0.72}, {'word': 'a', 'start': 24.645, 'end': 24.665, 'score': 0.753}, {'word': 'des', 'start': 24.705, 'end': 24.766, 'score': 0.83}, {'word': 'sketchs', 'start': 24.786, 'end': 25.026, 'score': 0.365}, {'word': 'que', 'start': 25.046, 'end': 25.126, 'score': 0.81}, {'word': "j'ai", 'start': 25.166, 'end': 25.246, 'score': 0.88}, {'word': 'faits,', 'start': 25.286, 'end': 25.386, 'score': 0.559}, {'word': 'ouais,', 'start': 25.426, 'end': 25.606, 'score': 0.375}, {'word': 'pas', 'start': 25.626, 'end': 25.706, 'score': 0.356}, {'word': 'ouf.', 'start': 25.726, 'end': 25.786, 'score': 0.014}]}, {'start': 25.806, 'end': 29.949, 'text': "Ou qui manquaient soit de finesse, soit de trucs, et t'as trouvé que t'étais pas... Et puis ça répondait à une époque,", 'words': [{'word': 'Ou', 'start': 25.806, 'end': 25.866, 'score': 0.143}, {'word': 'qui', 'start': 25.886, 'end': 26.006, 'score': 0.776}, {'word': 'manquaient', 'start': 26.046, 'end': 26.387, 'score': 0.601}, {'word': 'soit', 'start': 26.407, 'end': 26.527, 'score': 0.842}, {'word': 'de', 'start': 26.547, 'end': 26.607, 'score': 0.712}, {'word': 'finesse,', 'start': 26.627, 'end': 26.867, 'score': 0.679}, {'word': 'soit', 'start': 26.887, 'end': 27.007, 'score': 0.836}, {'word': 'de', 'start': 27.027, 'end': 27.067, 'score': 0.317}, {'word': 'trucs,', 'start': 27.087, 'end': 27.187, 'score': 0.19}, {'word': 'et', 'start': 27.227, 'end': 27.287, 'score': 0.04}, {'word': "t'as", 'start': 27.347, 'end': 27.467, 'score': 0.423}, {'word': 'trouvé', 'start': 27.567, 'end': 28.148, 'score': 0.461}, {'word': 'que', 'start': 28.168, 'end': 28.248, 'score': 0.361}, {'word': "t'étais", 'start': 28.268, 'end': 28.528, 'score': 0.323}, {'word': 'pas...', 'start': 28.548, 'end': 28.788, 'score': 0.305}, {'word': 'Et', 'start': 28.848, 'end': 28.928, 'score': 0.283}, {'word': 'puis', 'start': 28.968, 'end': 29.168, 'score': 0.731}, {'word': 'ça', 'start': 29.188, 'end': 29.289, 'score': 0.516}, {'word': 'répondait', 'start': 29.329, 'end': 29.629, 'score': 0.658}, {'word': 'à', 'start': 29.669, 'end': 29.689, 'score': 0.866}, {'word': 'une', 'start': 29.729, 'end': 29.809, 'score': 0.489}, {'word': 'époque,', 'start': 29.829, 'end': 29.949, 'score': 0.003}]}]
+styles = ["Default", "Tilte-left", "Tilte-right"]
+
+import random
+import os
+from typing import Iterator, TextIO
+import subprocess
+import datetime
+
+def filename(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def time_to_hhmmss(date):
+    #5.229 to 0:00:5.23
+    data = str(date)
+    second = int(date)
+    ms = int((date - second) * 100)
+    minutes = int(second) // 60
+    second = int(second) % 60
+    return f"00:{minutes}:{second}.{ms}"
+
+
+tab = []
+def write_ass(file: TextIO):
+    file.write("[Script Info]\n")
+    file.write("ScriptType: v4.00\n")
+    file.write("Collisions: Normal\n")
+    file.write("PlayDepth: 0\n")
+    file.write("\n")
+    file.write("[V4+ Styles]\n")
+    file.write("Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, TertiaryColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding, WrapStyle\n")
+    file.write("Style: Default, Arial, 21, &H00FF0000, &H000000FF, &H00000000, &H80000000, -1, 0, 0, 0, 100, 100, 0, 0.00, 1, 3, 1, 3, 30, 30, 30, 0, 2\n")
+    file.write("Style: Tilte-left, Arial, 21, &H00FF0000, &H000000FF, &H00000000, &H80000000, -1, 0, 0, 0, 100, 100, 0, 7.00, 1, 3, 1, 3, 30, 30, 30, 0, 2\n")
+    file.write("Style: Tilte-right, Arial, 21, &H00FF0000, &H000000FF, &H00000000, &H80000000, -1, 0, 0, 0, 100, 100, 0, -7.00, 1, 3, 1, 3, 30, 30, 30, 0, 2\n")
+    file.write("\n")
+    file.write("[Events]\n")
+    file.write("Format: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text\n")
+    
+    for s in words:
+        sentence_start = s['start']
+        sentence_end = s['end']
+        sentence_text = s['text']
+        for segment in s['words']:
+            word = segment['word']
+            if len(segment)==1:
+                break
+            start = segment['start']
+            end = segment['end']
+            tab.append([start,end,word])
+            
+            delta = (end - start) * 1000
+            boiler = "{\q1\\be1\\b700\shad10\\a11\k"+str(int(delta))+"}"
+            emoji = r" \{\frz345}\u1F468 "
+            text =boiler+word.upper().replace(" "," "+boiler)
+            style = styles[random.randint(0,2)]
+            file.write(f"""Dialogue: 0,{time_to_hhmmss(start)},{time_to_hhmmss(end)},{style},,50,50,20,,{text}"""+  "\n")
+            
+ass_path = "temp/"
+path = "input/test4.mp4"
+ass_path = os.path.join(ass_path, f"{filename(path)}.ass")
+with open(ass_path,"w", encoding="utf-8") as ass:
+            write_ass(file=ass)
+            
+def gen_video():
+        output_dir = "output/"
+        out_path = os.path.join(output_dir, f"{filename(path)}ASS.mp4")
+        print(f"Adding subtitles to {filename(path)}...")
+        ffmpeg_cmd = [
+            "ffmpeg",
+            "-i", path,
+            "-vf", f"ass={ass_path}",
+            "-c:a", "copy",
+            "-y",
+            out_path
+        ]
+        subprocess.run(ffmpeg_cmd, check=False)
+        
+
+gen_video()
