9930b20aac8847c68f25459d8a9bc2090842f5ff
newtondotcom <asphalt9fr@gmail.com>
2023-11-07 10:41:40 +0100

    add alignment for .ass

--- styles.py
+++ styles.py
@@ -1,4 +1,4 @@
-second_colors = ["&H00FFFFFF","&H00FFFFFF","&H00FFFFFF","&H0000FF00","&H00FF0000","&H0000FF00","&H000000FF"]
+second_colors = ["&H00FFFFFF","&H00FFFFFF","&H00FFFFFF","&H0000FF00","&H0000FF00","&H00FF0000","&H00FFFF00"]
 rotation = ["0.00","7.00","-7.00","0.00","0.00","0.00","0.00"]
 scales = ["100","110","90","100","100","100","100"]
 
@@ -9,7 +9,10 @@ def gen_styles():
         for j in rotation:
             for k in scales:
                 for l in scales:
-                    style = "Style: s" + str(count) + ", Arial, 22, " + i + ", &H0000FF00, &H00000000, &H00000000, -1, 0, 0, 0,"+k+","+l+", 0, " + j + ", 1, 3, 4, 3, 0, 0, 10, 0, 2\n" #30, 30, 30
+                    style = "Style: s" + str(count) + ", Arial, 22, " + i + ", &H00FFFFFF, &H00FFFFFF, &H00000000, -1, 0, 0, 0,"+k+","+l+", 0, " + j + ", 1, 3, 4, 3, 0, 0, 50, 0, 2\n" #30, 30, 30
                     styles.append(style)
                     count += 1
     return styles
+
+
+##Alignment values are based on the numeric keypad layout. {\an1} - bottom left, {\an2} - bottom center, {\an3} - bottom right, {\an4} - center left, {\an5} - center center, {\an6} - center right, {\an7} - top left, {\an8} - top center, {\an9} - top right.
