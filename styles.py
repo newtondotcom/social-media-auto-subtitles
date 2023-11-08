b429c2646323b6bdbb2f9c81fb99ab327dc6b7f8
newtondotcom <asphalt9fr@gmail.com>
2023-11-08 21:04:14 +0100

    rm useless styleing

--- styles.py
+++ styles.py
@@ -7,7 +7,7 @@ def gen_styles():
     for j in rotation:
         for k in scales:
             for l in scales:
-                style = "Style: s" + str(count) + ", Arial, 22, " + i[0] + ", &H0000FFFF, 1 , &H00000000 , -1, 0, 0, 0,"+k+","+l+", 0, " + j + ", 1, 3 , 4, 3, 0, 0, 50, 0, 2\n" #30, 30, 30
+                style = "Style: s" + str(count) + ", Arial, 22, &H00FFFFFF , &H0000FFFF, 1 , &H00000000 , -1, 0, 0, 0,"+k+","+l+", 0, " + j + ", 1, 3 , 4, 3, 0, 0, 50, 0, 2\n" #30, 30, 30
                 styles.append(style)
                 count += 1
     return styles
