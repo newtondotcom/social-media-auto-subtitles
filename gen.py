d7dee550dd4bfe86408c09c1b40428393c7a7ef7
newtondotcom <asphalt9fr@gmail.com>
2023-11-18 23:50:49 +0100

    more tests

--- gen.py
+++ gen.py
@@ -5,9 +5,11 @@ device = "cpu"  # "cuda" or "cpu"
 batch_size = 8 # reduce if low on GPU mem, before 16
 compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy), before float16
 
+
+model = whisperx.load_model("medium", device, compute_type=compute_type)
+
 def get_transcribe(audio_file):
-    # 1. Transcribe with original whisper (batched), medium model is "medium"
-    model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+    # 1. Transcribe with original whisper (batched), medium model is "medium", large model is "large-v2"
 
     audio = whisperx.load_audio(audio_file)
     result = model.transcribe(audio, batch_size=batch_size)
@@ -22,5 +24,6 @@ def get_transcribe(audio_file):
     # delete model if low on GPU resources
     # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
+    print(result['segments'])
     return result['segments']
 
