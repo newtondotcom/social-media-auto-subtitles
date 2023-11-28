290ab99d53799a5a7b6e77ce13a46a214ccf357f
newtondotcom <asphalt9fr@gmail.com>
2023-11-28 20:43:30 +0100

    s3 download

---
+++ s3.py
@@ -0,0 +1,18 @@
+import boto3
+
+# Replace these with your AWS credentials and S3 bucket and file information
+aws_access_key_id = 'your_access_key_id'
+aws_secret_access_key = 'your_secret_access_key'
+bucket_name = 'your_bucket_name'
+file_key = 's3.txt'
+local_file_path = 'local_path_to_save_downloaded_file.txt'
+
+# Create an S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+def download_file(file_key, local_file_path):
+    try:
+        s3.download_file(bucket_name, file_key, local_file_path)
+        print(f"File downloaded successfully to {local_file_path}")
+    except Exception as e:
+        print(f"Error downloading file: {e}")
