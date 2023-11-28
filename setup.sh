4378ce34fe23b14de9abc48fe6ecb3086458980d
newtondotcom <asphalt9fr@gmail.com>
2023-11-28 11:38:46 +0100

    new version with apt isntall git

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+apt install git
+apt install ffmpeg
 pip install git+https://github.com/m-bain/whisperx.git
 pip install ffmpeg-python
 pip install opencv-python
 pip install moviepy
-apt install ffmpeg
 mkdir temp
 mkdir output
